7c3414a76c4717f06c44ae2df713b818bba51e76
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-02-09 22:33:16 -0600

    Worked on server.js, connections, and models.

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'content',
+  }
+);
+
+module.exports = Post;
