b18e3e927fc5dba083e0c1bde0dbe097900275a2
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-02-11 16:09:09 -0600

    Wrote the models index.

--- models/Comment.js
+++ models/Comment.js
@@ -22,6 +22,13 @@ Comment.init(
         model: 'user',
         key: 'id',
       },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
     }
   },
   {
---
+++ models/index.js
@@ -0,0 +1,33 @@
+const Comment = require('./Comment')
+const Post = require('./Post')
+const User = require('./User')
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+module.exports = {
+  Comment, 
+  Post, 
+  User
+}
