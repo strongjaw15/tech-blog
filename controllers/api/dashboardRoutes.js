32b19b239eb17e11d8c21d1b0bb5692387a6573a
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-03-13 10:29:25 -0500

    Worked on dashboard.

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -5,15 +5,18 @@ const {Post, User, Comment} = require('../../models')
 
 router.get('/', authentication, async (req, res) => {
   try {
+    // This gets the user data from the db.
     const userData = await User.findOne({
       where: {id: req.session.user_id},
       include: [{model: Post}]
     })
 
+    // This serializes the data
     const userDataSerial = userData.get({plain: true})
     userDataSerial.posts = userDataSerial.posts.map((post) => {
       return {
         ...post,
+        // This formats the date.
         created_at: moment(post.created_at).format('MMMM Do YYYY, h:mm a')
       }
     })
@@ -27,5 +30,7 @@ router.get('/', authentication, async (req, res) => {
     res.status(500)
   }
 })
+
+router.post('/', authentication, async (req, res) => {})
 
 module.exports = router
